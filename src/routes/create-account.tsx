3b79c974bc06a26af780b4be098691bf0b677a08
elarge2025 <wpfhtm456@gmail.com>
2025-05-21 23:14:11 +0900

    10주차 과제

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,47 +1,10 @@
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
-import {styled} from "styled-components"
 import { auth } from "../firebase";
-import { useNavigate } from "react-router-dom";
-
-const Wrapper = styled.div`
-    height:100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 420px;
-    padding: 50px 0px;
-    `;
-
-const Title = styled.h1`
-    font-size: 42px;
-    `;
-
-const Form = styled.form`
-    margin-top: 50px;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    width: 100%;
-    `;
-
-const Input = styled.input`
-    padding: 10px 20px;
-    border-radius: 50px;
-    border: none;
-    width: 100%;
-    font-size: 16px;
-    &[type="submit"]{
-        cursor: pointer;
-        &:hover{
-            opacity: 0.8;
-        }
-    }`;
-
-const Error = styled.span`
-    font-weight: 600;
-    color: tomato;
-`;
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { Form ,Error, Input, Switcher, Title, Wrapper } from "../components/auth-components";
+import GithubButton from "../components/github-btn";
 
 export default function CreateAccount() {
     const navigate = useNavigate();
@@ -63,6 +26,7 @@ export default function CreateAccount() {
     };
     const onSubmit = async (e : React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setError("");
         console.log(name, email, password);
         if(isLoading || name === "" || email === "" || password === "") return;
         try{
@@ -74,7 +38,9 @@ export default function CreateAccount() {
            });
            navigate("/");
         } catch(e){
-
+            if(e instanceof FirebaseError){
+                setError(e.message);
+            }
         }
         finally{
             setLoading(false);
@@ -91,6 +57,10 @@ export default function CreateAccount() {
             />
         </Form>
         {error != "" ? <Error>{error}</Error> : null}
+        <Switcher>
+            이미 계정이 있으십니까? <Link to="/login">로그인 &rarr;</Link>
+        </Switcher>
+        <GithubButton />
     </Wrapper>
     );
 };
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,58 @@
-export default function Login() {
-    return <h1>Login </h1>;
-}
+import { useState } from "react";
+import { auth } from "../firebase";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { Form, Error, Input, Switcher, Title, Wrapper } from "../components/auth-components";
+import GithubButton from "../components/github-btn";
+
+export default function CreateAccount() {
+    const navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false);
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const onChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+        const{target:{name, value} }=e;
+        if(name==="email"){
+            setEmail(value);
+        }
+        else if(name==="password"){
+            setPassword(value);
+        }
+    };
+    const onSubmit = async (e : React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setError("");
+        console.log(name, email, password);
+        if(isLoading ||  email === "" || password === "") return;
+        try{
+            setLoading(true);
+            await signInWithEmailAndPassword(auth, email, password);
+           navigate("/");
+        } catch(e){
+            if(e instanceof FirebaseError){
+                setError(e.message);
+            }
+        }
+        finally{
+            setLoading(false);
+        }
+        console.log(name, email, password);
+    }
+    return (<Wrapper>
+        <Title>Log into 🌚</Title>
+        <Form onSubmit={onSubmit}>
+            <Input onChange = {onChange} name="email" value={email} placeholder="Email" type="email" required/>
+            <Input value={password} onChange = {onChange} name="password" placeholder="Password" type="password" required/>
+            <Input type="submit" value={isLoading ? "Loading..." : "Log in"}
+            />
+        </Form>
+        {error != "" ? <Error>{error}</Error> : null}
+        <Switcher>
+            계정이 없으십니까? <Link to="/create-account">계정 만들기 &rarr;</Link>
+        </Switcher>
+        <GithubButton />
+    </Wrapper>
+    );
+};
